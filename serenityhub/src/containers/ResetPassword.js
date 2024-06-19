22740eae93f2854c7abb3665bcfc85f9e23dbd80
lekakenycollins8 <lekakenycollins8@gmail.com>
2024-06-19 18:54:48 +0300

    setup djoser configurations in settings and activated the react app for front-end

---
+++ serenityhub/src/containers/ResetPassword.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ResetPassword = () => {
+        <div>
+                Reset
+        </div>
+};
+
+export default ResetPassword;
---
+++ serenityhub/src/containers/ResetPasswordConfirm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ResetPasswordConfirm = () => {
+        <div>
+                Confirm
+        </div>
+};
+
+export default ResetPasswordConfirm;
